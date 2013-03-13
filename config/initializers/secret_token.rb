b984666a65f408a1b168f945970f784895c40ac6
Jason Croci <jason_croci@yahoo.com>
2013-03-12 21:12:46 -0400

    Initial repos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virtualgamer::Application.config.secret_token = '4a9287551d1b7004c037dd63135fbeec841c4f150dbc128b0eee869cc5440840fb8f408ffb845e7388d00e594c586a2b7f7a0a0802f288342a7760b78dd6bdd2'
